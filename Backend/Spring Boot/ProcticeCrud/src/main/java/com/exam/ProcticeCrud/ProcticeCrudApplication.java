9388f9640d54269b1fcb3d3db037fe8deee8cfba
Sivanithi <sivacareer2001@gmail.com>
2023-02-23 12:59:38 +0530

    initiate practice project

---
+++ Backend/Spring Boot/ProcticeCrud/src/main/java/com/exam/ProcticeCrud/ProcticeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.exam.ProcticeCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcticeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcticeCrudApplication.class, args);
+	}
+
+}
---
+++ Backend/Spring Boot/ProcticeCrud/src/test/java/com/exam/ProcticeCrud/ProcticeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.ProcticeCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcticeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
