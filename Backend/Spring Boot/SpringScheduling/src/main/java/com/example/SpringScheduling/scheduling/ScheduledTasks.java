2a5c6cc624c85de7249fc561614196943babe009
Sivanithi <sivacareer2001@gmail.com>
2023-03-02 12:17:51 +0530

    Scheduling sample commit

--- Backend/Spring Boot/SpringScheduling/src/main/java/com/example/SpringScheduling/scheduling/ScheduledTasks.java
+++ Backend/Spring Boot/SpringScheduling/src/main/java/com/example/SpringScheduling/scheduling/ScheduledTasks.java
@@ -11,12 +11,12 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 
-    @Scheduled(fixedRate = 10000)
+    @Scheduled(fixedRate = 1000)
     public void performTask() {
         System.out.println("Regular task performed at" + dateFormat.format(new Date()));
     }
 
-    @Scheduled(initialDelay = 1000, fixedRate = 10000)
+    @Scheduled(initialDelay = 1000, fixedRate = 1000)
     public void performDelayedTask() {
         System.out.println("Delayed Regular performed at" + dateFormat.format(new Date()));
     }
