65671cd148a8243e3d90c95568641de4e76776ca
Sivanithi <sivacareer2001@gmail.com>
2023-03-20 16:06:42 +0530

    repo processed

--- Backend/Spring Boot/LoginPage/src/main/java/com/example/LoginPage/repository/RoleRepository.java
+++ Backend/Spring Boot/LoginPage/src/main/java/com/example/LoginPage/repository/RoleRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public interface RoleRepository extends MongoRepository<Role, String> {
 
-//    Optional<Role> findByName(ERole name);
+    Optional<Role> findByName(ERole name);
 
 }
