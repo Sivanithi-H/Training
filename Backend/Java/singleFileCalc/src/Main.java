b8859ff91dd762da5dc692c8765c374f97a49644
Sivanithi <sivacareer2001@gmail.com>
2023-04-10 14:38:06 +0530

    Looping system changed

--- Backend/Java/singleFileCalc/src/Main.java
+++ Backend/Java/singleFileCalc/src/Main.java
@@ -2,40 +2,54 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Scanner sc = new Scanner(System.in);
-        int a, b;
-        int option;
-        int result;
-        System.out.println("Enter Option :");
-        System.out.println("1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("3. Multiplication");
-        System.out.println("4. Division");
-        option = sc.nextInt();
-        System.out.println("Enter a : ");
-        a = sc.nextInt();
-        System.out.println("Enter b : ");
-        b = sc.nextInt();
-        switch (option) {
-            case 1 :
-                result = a + b;
-                System.out.println("Result : " +result);
-                break;
-            case 2 :
-                result = a - b;
-                System.out.println("Result : " +result);
-                break;
-            case 3 :
-                result = a * b;
-                System.out.println("Result : " +result);
-            case 4 :
-                try {
-                    result = a / b;
-                    System.out.println("Result : " +result);
-                } catch (ArithmeticException e) {
-                     System.out.println("ERROR : " +e.getMessage());
+        int option = 0;
+        boolean con = true;
+        boolean opr = true;
+
+        while (con) {
+            int result = 0;
+            con = false;
+            System.out.println("Enter a : ");
+            int a = sc.nextInt();
+            System.out.println("Enter b : ");
+            int b = sc.nextInt();
+
+            if (opr) {
+                System.out.println("Enter Option :");
+                System.out.println("1. Addition");
+                System.out.println("2. Subtraction");
+                System.out.println("3. Multiplication");
+                System.out.println("4. Division");
+                option = sc.nextInt();
+            }
+
+            if (option == 1 || option == 2 || option == 3 || option == 4) {
+
+                if (option == 1) {
+                    result = a + b;
                 }
+                if (option == 2) {
+                    result = a - b;
+                }
+                if (option == 3) {
+                    result = a * b;
+                }
+                if (option == 4) {
+                    try {
+                        result = a / b;
+                    } catch (ArithmeticException e) {
+                        System.out.println("Error : " + e.getMessage());
+                        con = true;
+                        opr = false;
+                    }
+                }
+
+            }else {
+                System.out.println("Invalid Operation");
+            }
+
+            System.out.println("Result : " +result);
         }
     }
 }
--- Backend/Java/singleFileCalc/src/RCalc.java
+++ Backend/Java/singleFileCalc/src/RCalc.java
@@ -6,16 +6,15 @@ public class RCalc {
         Scanner sc = new Scanner(System.in);
         int option;
         boolean con = true;
+        System.out.println("Enter Option :" );
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        option = sc.nextInt();
         while (con) {
             int result = 0;
             con = false;
-            System.out.println("Enter Option :" );
-            System.out.println("1. Addition");
-            System.out.println("2. Subtraction");
-            System.out.println("3. Multiplication");
-            System.out.println("4. Division");
-            option = sc.nextInt();
-
             if (option == 1 || option == 2 || option == 3 || option == 4) {
                 System.out.println("Enter a : ");
                 int a = sc.nextInt();
@@ -23,20 +22,16 @@ public class RCalc {
                 int b = sc.nextInt();
                 if (option == 1) {
                     result = a + b;
-//                    con = false;
                 }
                 if (option == 2) {
                     result = a - b;
-//                    con = false;
                 }
                 if (option == 3) {
                     result = a * b;
-//                    con = false;
                 }
                 if (option == 4) {
                     try {
                         result = a / b;
-//                        con = false;
                     }catch (ArithmeticException e) {
                         System.out.println("Error : " + e.getMessage());
                         con = true;
