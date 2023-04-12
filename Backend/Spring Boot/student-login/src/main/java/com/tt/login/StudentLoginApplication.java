8102fc3aa6c44601135c6b8ae0021ca3d89c5b4b
Sivanithi <sivacareer2001@gmail.com>
2023-04-12 11:12:23 +0530

    initial commit

---
+++ Backend/Spring Boot/student-login/src/main/java/com/tt/login/StudentLoginApplication.java
@@ -0,0 +1,13 @@
+package com.tt.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLoginApplication.class, args);
+	}
+
+}
---
+++ Backend/Spring Boot/student-login/src/test/java/com/tt/login/StudentLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
