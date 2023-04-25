0552da0b12eb6f676f7947f93eefcb1f4248cce4
Sivanithi <sivacareer2001@gmail.com>
2023-04-25 11:43:42 +0530

    Initial Commit

---
+++ Backend/Spring Boot/stringToken/src/main/java/com/stringToken/StringTokenApplication.java
@@ -0,0 +1,13 @@
+package com.stringToken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringTokenApplication.class, args);
+	}
+
+}
